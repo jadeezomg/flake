1349ee00fd08cb20e0bb9e1d937fa94efffcbb39
jadee <me@jadee.fyi>
2025-12-08 19:46:46 +0100

    fix pkg name

--- modules/shared/utils/monitoring.nix
+++ modules/shared/utils/monitoring.nix
@@ -3,7 +3,7 @@
   environment.systemPackages = with pkgs; [
     bottom # Better htop alternative
     btop # Better htop alternative
-    glxinfo # Info for OpenGL & Mesa
+    mesa-demos # Info for OpenGL & Mesa
     hyperfine # Command-line benchmarking tool
     nmon # System monitoring tool
     psmisc # killall, pstree, etc.
