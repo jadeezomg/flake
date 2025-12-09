bd719267eecdaab954876eed05c7496ffc41af6d
jadee <me@jadee.fyi>
2025-12-09 01:20:52 +0100

    update lock

---
+++ modules/shared/enviroment/nix.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  nix.settings = {
+    download-buffer-size = 524288000; # 500 MiB
+  };
+  # Nix experimental features
+  experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+}
