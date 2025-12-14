aed61abd8bc3d5ad339759cbddb9c084cbe9759e
jadee <me@jadee.fyi>
2025-12-14 19:33:44 +0100

    cleanup & imports

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -2,6 +2,23 @@
 
 {
   imports = [
-    ./compat.nix
+    ./apps
+    ./boot
+    ./desktop
+    ./development
+    ./environment
+    ./fonts
+    ./hardware
+    ./integration
+    ./locale
+    ./maintenance
+    ./networking
+    ./programs
+    ./security
+    ./services
+    ./shell
+    ./shells
+    ./utils
+    ./virtualization
   ];
 }
