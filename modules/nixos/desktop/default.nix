f7eebc174056bcfc12cb1fccbd4b6c7363114a61
jadee <me@jadee.fyi>
2025-12-14 21:38:03 +0100

    fm to nixos

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 
 {
+
+  imports = [
+    ./thunar.nix
+  ];
+
   # X server configuration (required for GDM and XWayland compatibility)
   # GNOME uses Wayland by default, but X server is needed for:
   # - GDM display manager
   # - XWayland (for running X11 applications)
+
   services = {
+    gvfs.enable = true; # Mount, trash, etc
+    tumbler.enable = true; # Thumbnail support for images
     xserver = {
       enable = true;
       # Configure keyboard layout (works for both Wayland and X11)
--- modules/shared/fonts/default.nix
+++ modules/shared/fonts/default.nix
@@ -1,9 +1,68 @@
 { pkgs, ... }:
-
 {
   fonts.packages = with pkgs; [
-    nerd-fonts.zed-mono
+    # Monospace
+    fira-code
+    fira-code-symbols
+    nerd-fonts.fira-code
+
+    fira-mono
+    nerd-fonts.fira-mono
+
+    hack-font
+    nerd-fonts.hack
+
+    ibm-plex
+    nerd-fonts.blex-mono
+
+    jetbrains-mono
+    nerd-fonts.jetbrains-mono
+
+    julia-mono
+    roboto-mono
+    nerd-fonts.roboto-mono
+
+    office-code-pro
+    inconsolata
+
+    cascadia-code
+    nerd-fonts.caskaydia-cove
+
+    # Iosevka Family
+    iosevka
     nerd-fonts.iosevka
+    nerd-fonts.iosevka-term
     nerd-fonts.iosevka-term-slab
+    nerd-fonts.zed-mono
+
+    paratype-pt-mono
+
+    # Sans-serif
+    liberation_ttf
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji-blob-bin
+    work-sans
+    roboto
+    raleway
+    quicksand
+    lato
+    dosis
+    open-sans
+    montserrat
+    source-sans-pro
+    libre-franklin
+
+    # Serif
+    cardo
+    merriweather
+    garamond-libre
+    crimson
+    gelasio
+
+    # Symbols
+    font-awesome
+    powerline-fonts
+    powerline-symbols
   ];
 }
