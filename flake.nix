3b6c8f8439480a6bc40068285e123442a3d05737
jadee <me@jadee.fyi>
2025-12-14 20:20:07 +0100

    zen branch

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
 
     zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
+      url = "github:0xc000022070/zen-browser-flake/beta";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
