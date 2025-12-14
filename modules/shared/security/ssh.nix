a8ab6dd6a9680eee02925e6fc48f3151d70e101f
jadee <me@jadee.fyi>
2025-12-14 19:58:30 +0100

    disable ssh while on gnome

--- modules/shared/security/ssh.nix
+++ modules/shared/security/ssh.nix
@@ -1,17 +1,17 @@
 { ... }:
 {
-  services.openssh = {
-    enable = true;
-    ports = [ 22 ];
-    openFirewall = true;
-    settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
-      PubkeyAuthentication = true;
-    };
-  };
+  # services.openssh = {
+  #   enable = true;
+  #   ports = [ 22 ];
+  #   openFirewall = true;
+  #   settings = {
+  #     PermitRootLogin = "no";
+  #     PasswordAuthentication = false;
+  #     PubkeyAuthentication = true;
+  #   };
+  # };
 
-  programs.ssh = {
-    startAgent = true;
-  };
+  # programs.ssh = {
+  #   startAgent = true;
+  # };
 }
