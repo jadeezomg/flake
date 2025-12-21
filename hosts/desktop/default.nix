2a67400c4a5fca93c0af324441d781bfe769c0a9
jadee <me@jadee.fyi>
2025-12-21 02:33:21 +0100

    cuda for desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -28,10 +28,18 @@ in {
   # System state version - host specific, don't change, it's used by home-manager to determine the initial version of the system.
   system.stateVersion = "25.11";
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://cache.nixos-cuda.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
+    ];
+  };
 
   maintenance.garbageCollection = {
     enable = true;
