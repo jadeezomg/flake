079cd639e1de370eb2cbf2fbcbcd11124cf50d84
jadee <me@jadee.fyi>
2026-03-02 22:38:25 +0100

    mangohud as hm

--- home/nixos/apps/default.nix
+++ home/nixos/apps/default.nix
@@ -1,9 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./audio
     ./terminals
     ./files
   ];
+
+  programs.mangohud.enable = true;
 
   home.packages = with pkgs; [
     protonmail-desktop
