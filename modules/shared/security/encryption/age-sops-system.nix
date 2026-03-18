118c30e229bcc37e3cb0b6c19bd3975147370c93
jadee <me@jadee.fyi>
2026-03-18 20:40:39 +0100

    fix sops

--- modules/shared/security/encryption/age-sops-system.nix
+++ modules/shared/security/encryption/age-sops-system.nix
@@ -5,11 +5,11 @@
   ...
 }: let
   userHome =
-    (config.users.users.${user}.home or "/home/${user}");
+    config.users.users.${user}.home or "/home/${user}";
 
   # NixOS tends to define `group`; Darwin may not, so fall back to `user`.
   userGroupOpt =
-    (config.users.users.${user}.group or null);
+    config.users.users.${user}.group or null;
 in {
   # Decrypt secrets using SOPS + age (sops-nix).
   #
@@ -26,15 +26,15 @@ in {
       keyFile = "${userHome}/.config/sops/age/keys.txt";
     };
 
-    secrets."github-token" = {
-      key = "github_token";
-      path = "${userHome}/.config/nix/github-token";
-      owner = user;
-      mode = "0400";
-    }
-    // lib.optionalAttrs (userGroupOpt != null) {
-      secrets."github-token".group = userGroupOpt;
-    };
+    secrets.github-token =
+      {
+        key = "github_token";
+        path = "${userHome}/.config/nix/github-token";
+        owner = user;
+        mode = "0400";
+      }
+      // lib.optionalAttrs (userGroupOpt != null) {
+        group = userGroupOpt;
+      };
   };
 }
-
