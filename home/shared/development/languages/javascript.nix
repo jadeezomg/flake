59a05b2561837e8170c714c4da8d55b7fe9b10a8
jadee <me@jadee.fyi>
2026-04-21 01:46:11 +0200

    more cli tools

--- home/shared/development/languages/javascript.nix
+++ home/shared/development/languages/javascript.nix
@@ -6,12 +6,8 @@
   home.packages = with pkgs; [
     # --- Javascript/Typescript/React.js/Next.js ---
     nodejs_24 # Node.js JavaScript runtime
-    #nodejs_22
-    #eslint
-    # npm
-    # prettier
-    # google-clasp
     bun
+    mise
     # Kagi client library (pair with kagi-ken-cli in tooling/llm.nix)
     (pkgs.callPackage ../../../../packages/kagi-ken {})
   ];
