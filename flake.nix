45fb2a4279b7ff5fd881274d0e97f417d1e463ed
jadee <me@jadee.fyi>
2026-04-26 17:10:41 +0200

    update and dms master

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
     };
 
     dms = {
-      url = "github:AvengeMedia/DankMaterialShell/stable";
+      url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
