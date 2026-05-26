5cc13660441ce8ca752e8a47cf731586b214cb49
jadeezomg <github@jadee.fyi>
2026-05-26 02:10:27 +0200

    gvfs fix, niri, fmt

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -11,7 +11,7 @@
   fileSystems."/mnt/storage" = {
     device = "/dev/disk/by-uuid/777dc8ab-1fe3-4b38-b6e2-9976491ce434";
     fsType = "ext4";
-    options = ["defaults"];
+    options = ["defaults" "x-gvfs-show"];
   };
 
   programs.coolercontrol = {
