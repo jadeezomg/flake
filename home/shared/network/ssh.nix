c042e9d40b8ed657c9234a9c25f73b76c9e0ead0
jadee <github@jadee.fyi>
2026-05-29 20:52:44 +0200

    more

--- home/shared/network/ssh.nix
+++ home/shared/network/ssh.nix
@@ -1,13 +1,15 @@
 {lib, ...}: let
   sshDestinations = (import ../../../data/network/ssh-destinations.nix).destinations;
-  hostBlocks = lib.mapAttrs (_alias: {
-    hostName,
-    user,
-    ...
-  }: {
-    HostName = hostName;
-    User = user;
-  }) sshDestinations;
+  hostBlocks =
+    lib.mapAttrs (_alias: {
+      hostName,
+      user,
+      ...
+    }: {
+      HostName = hostName;
+      User = user;
+    })
+    sshDestinations;
 in {
   programs.ssh = {
     enable = true;
